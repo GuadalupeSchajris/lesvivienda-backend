da9267cb6336314ff40d0b90afae82bc0c16798f
Guadalupe <haniguadalupe@gmail.com>
2025-05-22 18:01:46 +0200

    INITIALIZER

---
+++ src/test/java/com/lesvivienda/lesvivienda/LesviviendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesvivienda.lesvivienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesviviendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
